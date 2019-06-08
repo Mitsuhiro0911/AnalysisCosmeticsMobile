2be9feb78650bf73a8ec97d04e3cf8dd3699bd11
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-06-08 14:36:32 +0900

    読み込んだXMLファイルを操作する処理

--- app/src/main/java/com/example/analysiscosmeticsmobile/MainActivity.kt
+++ app/src/main/java/com/example/analysiscosmeticsmobile/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.analysiscosmeticsmobile
 
+import android.content.Context
 import android.content.res.AssetManager
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.analysiscosmeticsmobile.Constants.Companion.reader
 import java.io.InputStream
+import android.content.res.XmlResourceParser
+import android.R.xml
+import org.dom4j.io.SAXReader
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.xpath.XPathFactory
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +23,14 @@ class MainActivity : AppCompatActivity() {
         val cal = Calculator()
         val write = Write()
         // コーパスの商品数を計算
-        val asset = assets.open("cosme_product.xml")
-        val str = asset.reader(charset=Charsets.UTF_8).use{
-            it.readText()
-        }
+
+        val xml: InputStream = assets.open("cosme_product.xml")
+        val f = XPathFactory.newInstance()
+        val path = f.newXPath()
+        val reader = SAXReader()
+        val document = reader.read(xml)
+        Log.d("MainActivity", document.selectNodes("//product").count().toString())
+
 //        val assetManager: AssetManager = getResources().getAssets()
 //        var xml = assetManager.openXmlResourceParser("cosme_product.xml");
 //        val productNum = cal.calProductNum()
