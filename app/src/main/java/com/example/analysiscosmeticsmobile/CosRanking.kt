195349d65215b3218a710c0a471ef9da4462dfd5
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-06-15 13:48:05 +0900

    Main画面で選択した商品とその他商品のコサイン類似度をCosRanking画面で計算する処理を追加

--- app/src/main/java/com/example/analysiscosmeticsmobile/CosRanking.kt
+++ app/src/main/java/com/example/analysiscosmeticsmobile/CosRanking.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.content.Intent
 import android.nfc.NfcAdapter.EXTRA_DATA
 import android.util.Log
+import net.sf.javaml.tools.InstanceTools.array
 
 
 class CosRanking : AppCompatActivity() {
@@ -15,7 +16,19 @@ class CosRanking : AppCompatActivity() {
 
         val checkedId = intent.getIntExtra("EXTRA_DATA", -1)
         Log.d("cosranking", "${checkedId}")
+        val cal = Calculator()
+        val cosArray = mutableListOf<Int>()
+        val vector1 = Data.productMapList.get(checkedId).values.toDoubleArray()
         for(i in 0..10){
+            if(i == checkedId){
+                cosArray.add(0)
+            } else {
+                val vector2 = Data.productMapList.get(i).values.toDoubleArray()
+                cosArray.add((cal.calCosSimilarity(vector1, vector2) * Data.NORM).toInt())
+            }
+        }
+        for(cos in cosArray){
+            Log.d("cosarray", "${cos}")
         }
     }
 }
