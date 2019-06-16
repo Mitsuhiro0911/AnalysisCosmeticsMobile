1562fd3eb1e7ed76ddd4123bf97e3427c2021adb
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-06-16 16:54:43 +0900

    タブのアクティビティをメインアクティビティにするため、各種ファイル名をリファクタリング

--- app/src/main/java/com/example/analysiscosmeticsmobile/MainActivity.kt
+++ app/src/main/java/com/example/analysiscosmeticsmobile/MainActivity.kt
@@ -1,45 +1,28 @@
 package com.example.analysiscosmeticsmobile
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import org.dom4j.io.SAXReader
-import android.content.Intent
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import com.example.analysiscosmeticsmobile.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        // オブジェクト生成
-        val reader = SAXReader()
-        val cosmeProductCorpas = reader.read(assets.open("cosme_product.xml"))
-        val cosmeComponentDictionary = reader.read(assets.open("cosme_component_dictionary.xml"))
-        Data().setData(cosmeProductCorpas, cosmeComponentDictionary)
-
-        var radioGroup = findViewById<View>(R.id.radioGroup) as RadioGroup
-        // ラジオボタンを商品数分だけ動的に生成
-        for(i in 0 until Data.productNameList.size){
-            val radio = RadioButton(this)
-            // ラジオボタンのテキストに商品名を入れる
-            radio.text = Data.productNameList.get(i).text
-            // ラジオボタンのidを動的に生成
-            radio.id = i
-            radioGroup.addView(radio)
-        }
-
-        val executeButton = findViewById<View>(R.id.executeButton)
-        executeButton.setOnClickListener {
-            // 選択されたラジオボタンのidを取得
-            val checkedId = radioGroup.getCheckedRadioButtonId()
-            Log.d("checked", "${checkedId}")
-            val intent = Intent(this, CosRanking::class.java)
-            intent.putExtra("CHECKED_ID", checkedId);
-            startActivity(intent)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 }
---
+++ app/src/main/java/com/example/analysiscosmeticsmobile/TempMainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.analysiscosmeticsmobile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import org.dom4j.io.SAXReader
+import android.content.Intent
+
+class TempMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.temp_activity_main)
+
+        // オブジェクト生成
+        val reader = SAXReader()
+        val cosmeProductCorpas = reader.read(assets.open("cosme_product.xml"))
+        val cosmeComponentDictionary = reader.read(assets.open("cosme_component_dictionary.xml"))
+        Data().setData(cosmeProductCorpas, cosmeComponentDictionary)
+
+        var radioGroup = findViewById<View>(R.id.radioGroup) as RadioGroup
+        // ラジオボタンを商品数分だけ動的に生成
+        for(i in 0 until Data.productNameList.size){
+            val radio = RadioButton(this)
+            // ラジオボタンのテキストに商品名を入れる
+            radio.text = Data.productNameList.get(i).text
+            // ラジオボタンのidを動的に生成
+            radio.id = i
+            radioGroup.addView(radio)
+        }
+
+        val executeButton = findViewById<View>(R.id.executeButton)
+        executeButton.setOnClickListener {
+            // 選択されたラジオボタンのidを取得
+            val checkedId = radioGroup.getCheckedRadioButtonId()
+            Log.d("checked", "${checkedId}")
+            val intent = Intent(this, CosRanking::class.java)
+            intent.putExtra("CHECKED_ID", checkedId);
+            startActivity(intent)
+        }
+    }
+}
