607de87ecc8de1bafc138822cb454c4a6c9cbfee
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-06-22 16:04:25 +0900

    新規タブ(商品情報)を追加。

--- app/src/main/java/com/example/analysiscosmeticsmobile/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/analysiscosmeticsmobile/ui/main/SectionsPagerAdapter.kt
@@ -8,7 +8,8 @@ import com.example.analysiscosmeticsmobile.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
@@ -29,6 +30,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : 
 
     override fun getCount(): Int {
         // Show 2 total pages.
-        return 2
+        return 3
     }
 }
